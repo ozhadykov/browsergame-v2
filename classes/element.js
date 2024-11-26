e13b584f33f1947c598164b346df6863054ea1db
Omar Zhadykov <omar3282088@gmail.com>
2024-11-26 23:09:57 +0100

    decided to implement drawing function for platforms and background and player in own classes withot one solution for all

--- classes/element.js
+++ classes/element.js
@@ -7,66 +7,23 @@ export default class BaseGameElement {
    * @param height
    * @param width
    * @param scale
-   * @param frame
-   * @param frameBuffer
-   * @param cropBoxPosition
    */
-  constructor(
-    {
-      position,
-      imageSrc,
-      height,
-      width,
-      scale = 1,
-      frame = 1,
-      frameBuffer = 3,
-      cropBoxPosition = {x: 0, y: 0}
-    }
-  ) {
+  constructor({ position, height, width })
+  {
     this.height = height;
-      this.width = width;
+    this.width = width;
     this.position = position;
-    this.scale = scale;
-
-    this.frame = frame;
-    this.image = new Image();
-    this.image.onload = () => {
-      // this.height = height;
-      // this.width = width;
-    }
-    this.image.src = imageSrc;
-    this.currentFrame = 0;
-    this.frameBuffer = frameBuffer;
-    this.elapsedFrames = 0
-    this.cropBox = {
-      height: 16,
-      width: 16,
-      position: cropBoxPosition
-    }
   }
 
-  action() {
+  action()
+  {
   }
 
-  draw(ctx, r, g, b, a, x, y, width, height) {
+  draw(ctx, r, g, b, a, x, y, width, height)
+  {
     ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`
     ctx.fillRect(x, y, width, height)
     ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`
-
-    if (!this.image) return
-
-    ctx.drawImage(
-      this.image,
-      this.cropBox.position.x,
-      this.cropBox.position.y,
-      this.cropBox.height,
-      this.cropBox.width,
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
-    )
   }
-
 
 }
--- classes/platform.js
+++ classes/platform.js
@@ -2,11 +2,47 @@ import BaseGameElement from "./element.js";
 
 export class Platform extends BaseGameElement {
 
-  constructor(params) {
-    super(params)
+  constructor(
+    {
+      position,
+      height,
+      width,
+      imageSrc,
+      cropBoxPosition = {x: 0, y: 0},
+    }
+  ) {
+
+    super({position, height, width})
+
+    this.image = new Image();
+    this.image.onload = () => {
+      console.log('Image ready')
+    }
+    this.image.src = imageSrc;
+    this.cropBox = {
+      height: 16,
+      width: 16,
+      position: cropBoxPosition
+    }
   }
 
-  update() {
+  draw(ctx) {
+    if (!this.image) return
+
+    ctx.drawImage(
+      this.image,
+      this.cropBox.position.x,
+      this.cropBox.position.y,
+      this.cropBox.height,
+      this.cropBox.width,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    )
+  }
+
+  action() {
 
   }
 
