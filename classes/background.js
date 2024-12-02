4117ea4aadf8f41e1f93cbb790fa3872eddcbfaa
ID-9 <imke.deutschlaender@student.uni-halle.de>
2024-12-02 22:44:11 +0100

    Update background.js
    Update Constructor, draw function

--- classes/background.js
+++ classes/background.js
@@ -1,7 +1,33 @@
 import BaseGameElement from "./element.js";
 
 export class Background extends BaseGameElement {
-  constructor(params) {
-    super(params);
+  constructor(
+    {
+      position,
+      height,
+      width,
+      imageSrc,
+      scale = 1,
+     
+    }) {
+    super({position, height, width});
+    this.loaded = false
+    this.image = new Image();
+    this.image.onload = () => {
+      this.width = this.image.width * this.scale
+      this.height = this.image.height *this.scale
+      this.loaded = true
+      console.log('Image ready')
+    }
+    this.image.src = imageSrc;
+    this.scale = scale
+    }
+  
+
+  draw (ctx, r, g, b, a, x, y, width, height) 
+  { ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height); 
+         console.log('Image loaded and drawn') 
+  
   }
+
 }
