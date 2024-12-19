3b12998500c49002ed4b7b984f8efabb8a893993
Paul <paul.fricke@student.uni-halle.de>
2024-12-19 09:55:35 +0100

    png fixed v3

--- src/classes/goal.js
+++ src/classes/goal.js
@@ -1,5 +1,4 @@
 import { BaseElement } from "../base-classes";
-import { HitBox } from "./hit-box.js";
 
 export default class Goal extends BaseElement {
   constructor(
@@ -14,16 +13,16 @@ export default class Goal extends BaseElement {
       framesX,
       framesY,
     }) {
-    super({x, y, height, width, scale, imageSrc, imageCropBox, framesX, framesY });
-    }
-    checkForGoalReached(player){
-      if(
-      player._hitBox.getX() + player._hitBox.getWidth() >= this.getX() && player._hitBox.getY() + player._hitBox.getHeight()>= this.getY() &&
+    super({ x, y, height, width, scale, imageSrc, imageCropBox, framesX, framesY });
+  }
+  checkForGoalReached(player) {
+    if (
+      player._hitBox.getX() + player._hitBox.getWidth() >= this.getX() && player._hitBox.getY() + player._hitBox.getHeight() >= this.getY() &&
       player._hitBox.getX() <= this.getX() + this.getWidth() && player._hitBox.getY() <= this.getY() + this.getHeight()
-      ){
-        return true
-      }
-      
+    ) {
+      return true
     }
+
+  }
 
 }
