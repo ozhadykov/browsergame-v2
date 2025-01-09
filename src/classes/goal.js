5e06334d9cd5945b8b58af46d5f6e18d823b21a9
Paul <paul.fricke@student.uni-halle.de>
2025-01-09 08:59:30 +0100

    fixing

--- src/classes/goal.js
+++ src/classes/goal.js
@@ -12,7 +12,6 @@ export default class Goal extends BaseElement {
       framesX,
       framesY,
     }) {
-<<<<<<< HEAD
     super({ x, y, height, width, imageSrc, imageCropBox, framesX, framesY });
   }
   checkForGoalReached(player) {
@@ -25,18 +24,4 @@ export default class Goal extends BaseElement {
       return true
     }
   }
-=======
-    super({ x, y, height, width, scale, imageSrc, imageCropBox, framesX, framesY });
-  }
-  checkForGoalReached(player) {
-    if (
-      player._hitBox.getX() + player._hitBox.getWidth() >= this.getX() && player._hitBox.getY() + player._hitBox.getHeight() >= this.getY() &&
-      player._hitBox.getX() <= this.getX() + this.getWidth() && player._hitBox.getY() <= this.getY() + this.getHeight()
-    ) {
-      return true
-    }
-
-  }
-
->>>>>>> 3b12998500c49002ed4b7b984f8efabb8a893993
 }
